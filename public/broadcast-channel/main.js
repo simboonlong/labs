460b4bcc4186b929a7f42c4d4bb4886d96f3bba3
Sim Boon Long <simboonlong@gmail.com>
2022-05-11 21:29:11 +0800

    feat: broadcast channel

---
+++ public/broadcast-channel/main.js
@@ -0,0 +1,46 @@
+import { getSimpleUuid, onPostMessage, onMessageSubmit } from "./utils.js";
+
+let ID;
+
+const ROOM = "roomId";
+const message = document.getElementById("message");
+const name = document.getElementById("name");
+const iframe = document.getElementById("iframe");
+
+const url = new URL(location);
+const params = new URLSearchParams(url.search);
+
+const names = [
+  "John",
+  "Mary",
+  "Chris",
+  "Sophie",
+  "Peter",
+  "Anne",
+  "Nick",
+  "Carrie",
+  "Brandon",
+  "Jane",
+];
+
+const randomName = names[Math.floor(Math.random() * names.length)];
+name.setAttribute("value", randomName);
+
+if (params.has(ROOM)) {
+  ID = params.get(ROOM);
+} else {
+  ID = getSimpleUuid();
+
+  const SearchParams = searchParams();
+  SearchParams.update({ key: ROOM, value: ID });
+}
+
+const bc = new BroadcastChannel(`channel_${ID}`);
+iframe.setAttribute("src", `./iframe.html?roomId="${ID}"`);
+
+document.getElementById("button").addEventListener("click", () => {
+  onPostMessage(bc);
+});
+message.addEventListener("keyup", (e) => {
+  onMessageSubmit(bc, e.keyCode);
+});
